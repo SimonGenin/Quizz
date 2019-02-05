5da2381e2972014afb5bac653d2c4ae3bf8f8c7d
Simon Genin <simongenin@hotmail.fr>
2019-02-05 17:13:34 +0100

    Add auth testing

---
+++ tests/Feature/Auth/ForgotPasswordTest.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use Quizz\User;
+use Tests\TestCase;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Notification;
+use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ForgotPasswordTest extends TestCase {
+    use RefreshDatabase;
+
+    protected function passwordRequestRoute() {
+        return route('password.request');
+    }
+
+    protected function passwordEmailGetRoute() {
+        return route('password.email');
+    }
+
+    protected function passwordEmailPostRoute() {
+        return route('password.email');
+    }
+
+    protected function guestMiddlewareRoute() {
+        return route('home');
+    }
+
+    /** @test */
+    function user_can_view_an_email_password_form() {
+        $response = $this->get($this->passwordRequestRoute());
+
+        $response->assertSuccessful();
+        $response->assertViewIs('auth.passwords.email');
+    }
+
+    /** @test */
+    function user_cannot_view_an_email_password_form_when_authenticated() {
+        $user = factory(User::class)->make();
+
+        $response = $this->actingAs($user)->get($this->passwordRequestRoute());
+
+        $response->assertRedirect($this->guestMiddlewareRoute());
+    }
+
+    /** @test */
+    function user_receives_an_email_with_a_password_reset_link() {
+        Notification::fake();
+        $user = factory(User::class)->create([
+            'email' => 'john@example.com',
+        ]);
+
+        $response = $this->post($this->passwordEmailPostRoute(), [
+            'email' => 'john@example.com',
+        ]);
+
+        $this->assertNotNull($token = DB::table('password_resets')->first());
+        Notification::assertSentTo($user, ResetPassword::class, function ($notification, $channels) use ($token) {
+            return Hash::check($notification->token, $token->token) === true;
+        });
+    }
+
+    /** @test */
+    function user_does_not_receive_email_when_not_registered() {
+        Notification::fake();
+
+        $response = $this->from($this->passwordEmailGetRoute())->post($this->passwordEmailPostRoute(), [
+            'email' => 'nobody@example.com',
+        ]);
+
+        $response->assertRedirect($this->passwordEmailGetRoute());
+        $response->assertSessionHasErrors('email');
+        Notification::assertNotSentTo(factory(User::class)->make(['email' => 'nobody@example.com']), ResetPassword::class);
+    }
+
+    /** @test */
+    function email_is_required() {
+        $response = $this->from($this->passwordEmailGetRoute())->post($this->passwordEmailPostRoute(), []);
+
+        $response->assertRedirect($this->passwordEmailGetRoute());
+        $response->assertSessionHasErrors('email');
+    }
+
+    /** @test */
+    function email_is_a_valid_email() {
+        $response = $this->from($this->passwordEmailGetRoute())->post($this->passwordEmailPostRoute(), [
+            'email' => 'invalid-email',
+        ]);
+
+        $response->assertRedirect($this->passwordEmailGetRoute());
+        $response->assertSessionHasErrors('email');
+    }
+}
